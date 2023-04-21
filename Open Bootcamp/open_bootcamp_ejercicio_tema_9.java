4d1f5311d33470e65de531f48e8ebc62279df1ad
Alexandre Spindola <104132868+alexandrespindola@users.noreply.github.com>
2023-04-21 14:44:36 +0200

    Update open_bootcamp_ejercicio_tema_9.java

--- Open Bootcamp/open_bootcamp_ejercicio_tema_9.java
+++ Open Bootcamp/open_bootcamp_ejercicio_tema_9.java
@@ -5,17 +5,17 @@ public class Main {
     public static void main(String[] args) {
         Cliente cliente = new Cliente();
         Trabajador trabajador = new Trabajador();
-        cliente.nombre = "Raúl";
-        cliente.edad = 23;
-        cliente.telefono = 293214122;
-        cliente.credito = 150.55;
+        cliente.nombre = "Pablo";
+        cliente.edad = 34;
+        cliente.telefono = 642987445;
+        cliente.credito = 2987.23;
         System.out.println("Soy " + cliente.nombre + ", tengo " + cliente.edad + " años y mi telefono es " + cliente.telefono
                 + " y mi credito disponible es " + cliente.credito + "€");
 
-        trabajador.nombre = "Jose";
-        trabajador.edad = 42;
-        trabajador.telefono = 582910244;
-        trabajador.salario = 2000.32;
+        trabajador.nombre = "Juan";
+        trabajador.edad = 37;
+        trabajador.telefono = 643558472;
+        trabajador.salario = 3557.96;
         System.out.println("Soy " + trabajador.nombre + ", tengo " + trabajador.edad + " años y mi telefono es " + trabajador.telefono
                 + " y mi salario es de " + trabajador.salario + "€");
 
